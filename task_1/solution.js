030ed383a4ce7547998dd0cddd1817e3cf17453f
Tanya <hamster.bk@yandex.ru>
2022-03-26 21:59:29 +0500

    task №1 done

--- task_1/solution.js
+++ task_1/solution.js
@@ -14,7 +14,13 @@ function addInCartMessage(title, price) {
     // добавьте в конец переменной message текст 'теперь в корзине!'
     // выведите переменную message в консоль
 
+    let message;
+    message  = productName + ' ' + 'за' + ' ' + productPrice + ' ' + 'теперь в корзине!';
+
+console.log(message);
+
     // Конец решения задания №1.1.
+
 
     return message;
 }
@@ -27,6 +33,9 @@ function addInCartCountChange(value) {
     // создайте переменную newValue
     // присвойте ей значение oldValue
     // увеличьте значение newValue на 1
+
+    let newValue;
+    newValue = oldValue + 1;
 
     // Конец решения задания №1.2.
 
@@ -42,6 +51,11 @@ function addInCartSumChange(sum, delta) {
     // создайте переменную newSum и присвойте ей сумму oldSum и difference
     // создайте переменную newSumText и присвойте ей результат склеивания переменной newSum и текста ' ₽'
     // замените склеивание, используемое в newSum Text на шаблонную строку
+
+    let newSum;
+    newSum  = oldSum + difference;
+
+    let newSumText = `${newSum} ₽`;
 
     // Конец решения задания №1.3.
 
