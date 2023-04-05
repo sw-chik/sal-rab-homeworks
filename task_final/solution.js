f431e328f3aa6d6b8894b0762fdc93e3722a8751
sw-chik <hamster.bk@yandex.ru>
2023-04-05 21:41:36 +0300

    Update solution.js

--- task_final/solution.js
+++ task_final/solution.js
@@ -1,53 +1,69 @@
-// Исправьте функцию sendRequest
-// Аргументы функции:
-// - имя клиента
-// - телефон клиента
-// - объект с адресом доставки: {street, house, entrance, floor, flat}
-// - список товаров в заказе
-// - стоимость заказа с учетом скидок и доставки
-// Как результат функции требуется вернуть JSON,
-// cформированный в соответствии с правилами:
-// Объект data содержит все данные
-// В объекте data есть свойства:
-// - client - строка, имя клиента + телефон клиента;
-// - order - объект, содержащий данные о заказе:
-//     - address - строка с адресом доставки, записанным человекопонятным языком (как в примере)
-//     - sum - стоимость заказа с учетом скидок и доставки
-// - goods: массив объектов с информацией о позициях заказа:
-//     - title - название позиции
-//     - count - количество в заказе
-// например:
-// {
-//    "data": {
-//      "client": "Иван +7(987)65-43-210",
-//      "order": {
-//        "address": "ул. Ленина, дом 2, 4 подъезд, 5 этаж, кв 53",
-//        "sum": 900
-//       },
-//       "goods": [
-//         {
-//           "title": "Пицца",
-//           "count": 2
-//         }
-//      ]
-//    }
-// }
+/**
+ * Исправьте функцию sendRequest
+ * Аргументы функции:
+ * - имя клиента
+ * - телефон клиента
+ * - объект с адресом доставки: {street, house, entrance, floor, flat}
+ * - список товаров в заказе
+ * - стоимость заказа с учетом скидок и доставки
+ * Как результат функции требуется вернуть JSON,
+ * сформированный в соответствии с правилами:
+ * Объект data содержит все данные
+ * В объекте data есть свойства:
+ * - client - строка, имя клиента + телефон клиента;
+ * - order - объект, содержащий данные о заказе:
+ *     - address - строка с адресом доставки, записанным человекопонятным языком (как в примере)
+ *     - sum - стоимость заказа с учетом скидок и доставки
+ * - goods: массив объектов с информацией о позициях заказа:
+ *     - title - название позиции
+ *     - count - количество в заказе
+ * например:
+ * {
+ *    "data": {
+ *      "client": "Иван +7(987)65-43-210",
+ *      "order": {
+ *        "address": "ул. Ленина, дом 2, 4 подъезд, 5 этаж, кв 53",
+ *        "sum": 900
+ *       },
+ *       "goods": [
+ *         {
+ *           "title": "Пицца",
+ *           "count": 2
+ *         }
+ *      ]
+ *    }
+ * }
+ */
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
-
-    let countOfGoods = goods.length;
-
-    for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
+    let data = {
+        goods: [],
+        order: {
+            address: '',
+            sum: sum,
+        },
+        client: `${name} ${phone}`,
+    };
+    for (let i = 0; i < goods.length; i++) {
+        data.goods.push({
+            title: goods[i].title,
+            count: goods[i].count,
+        });
     }
-
-    data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
-
-    data.client = 'Иван';
-
-    let jsonData = JSON.stringify(data);
-
-    return jsonData;
+    if (address.street) {
+        data.order.address += `ул. ${address.street}`;
+    }
+    if (address.house) {
+        data.order.address += `, дом ${address.house}`;
+    }
+    if (address.entrance) {
+        data.order.address += `, ${address.entrance} подъезд`;
+    }
+    if (address.floor) {
+        data.order.address += `, ${address.floor} этаж`;
+    }
+    if (address.flat) {
+        data.order.address += `, кв ${address.flat}`;
+    }
+    return JSON.stringify({data: data});
 }
